efd4d579460e437f6b017c39d9c99e13d4f1eb0e
josephsmendoza <josephsmendoza@gmail.com>
2023-06-16 05:30:11 -0700

    don't fail when repo exists

--- gsn-install.sh
+++ gsn-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -xeuo pipefail
-git clone https://github.com/sugoidogo/gamescope-session-nested.git
+git clone https://github.com/sugoidogo/gamescope-session-nested.git || true
 cd gamescope-session-nested
+git pull
 cp -rv .local ~/
 chmod +x ~/.local/share/gamescope-session-nested/*
 update-desktop-database ~/.local/share/applications
