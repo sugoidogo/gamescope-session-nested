95e329e2bba646c5b8168df61a9038b1a6081b1e
SugoiDogo <josephsmendoza@gmail.com>
2023-06-16 06:53:16 -0700

    Update gamescope-session-nested.sh

--- .local/share/gamescope-session-nested/gamescope-session-nested.sh
+++ .local/share/gamescope-session-nested/gamescope-session-nested.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 if pgrep steam; then steam -shutdown; fi
 while pgrep steam; do sleep 1; done
-for pid in $(pgrep gamescope); do kill $pid; done
+pkill -x gamescope
 
 systemd-run --user --pty --pipe \
     --setenv GAMESCOPE="$(which gamescope)" \
