785e2d5beae17065ee967304890dccd6cbc8f036
josephsmendoza <josephsmendoza@gmail.com>
2023-06-18 02:23:02 -0700

    add chimeraos/gamescope-session support

--- .local/share/gamescope-session-nested/gamescope-session-nested.sh
+++ .local/share/gamescope-session-nested/gamescope-session-nested.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -x
+cd "$(dirname "$(readlink -f -- "$0")")"
 
 if pgrep -x steam; then steam -shutdown; fi
 while pgrep -x steam; do sleep 1; done
 pkill -xe gamescope || true
 
-systemd-run --user --pty --pipe \
-    --setenv GAMESCOPE="$(which gamescope)" \
-    --setenv PATH="~/.local/share/gamescope-session-nested:$PATH" \
-    gamescope-session
+chimera="/usr/share/gamescope-session/gamescope-session-script"
+if [ -f "$chimera" ] ; then
+    SESSIONCMD=$chimera
+else
+    SESSIONCMD='gamescope-session'
+fi
 
-systemd-run --user steam -silent
+systemd-run --user --pty --pipe \
+    --setenv DISPLAY=$DISPLAY \
+    --setenv WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+    --setenv PATH="$PWD:$PATH" \
+    $SESSIONCMD
+
+systemd-run --user \
+    --setenv DISPLAY=$DISPLAY \
+    --setenv WAYLAND_DISPLAY=$WAYLAND_DISPLAY \
+    steam -silent -nofriendsui -nochatui
