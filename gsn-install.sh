6fbaf6030c19b0c7db0c3e53ac4a592b64fdebe2
josephsmendoza <josephsmendoza@gmail.com>
2023-06-17 23:59:59 -0700

    add chimeraos/gamescope-session support

--- gsn-install.sh
+++ gsn-install.sh
@@ -3,7 +3,7 @@ set -xeuo pipefail
 git clone https://github.com/sugoidogo/gamescope-session-nested.git || true
 cd gamescope-session-nested
 git pull
-cp -rv .local ~/
+cp -rv .local .config ~/
 chmod +x ~/.local/share/gamescope-session-nested/*
 update-desktop-database ~/.local/share/applications
 read -rp "Install Complete"
